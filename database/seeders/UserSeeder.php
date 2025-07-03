ec6b561e7d4bc9070ac4e929ffd8e2df23964b5d
Dibyo178 <sourov.mu@gmail.com>
2025-07-03 17:51:04 +0600

    sedder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+// use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superAdmin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+
+        //  create user
+
+        $role = Role::create(['name' => 'Admin']);
+
+        // Assohn permission to role
+
+        $permission = Permission::pluck('id')->all();
+        $role->syncPermissions($permission);
+
+        // Assign role to user
+
+      $superAdmin->assignRole($role);
+      $admin->syncRoles($role);
+
+    }
+}
