cb750da156081b64f0f99be5a53b3b897b85e14f
Dibyo178 <Sourov.mu@gmail.com>
2025-07-05 00:29:57 +0600

    login

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,37 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('backend.pages.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+// route start
+
+Route::get('test',function(){
+
+    return view('backend.pages.auth.login');
+
+});
+
+Route::middleware(['auth', 'verified'])->group(function(){
+
+    Route::get('user-logout',[UserController::class,'logout'])->name('user-logout');
+
 });
 
 require __DIR__.'/auth.php';
